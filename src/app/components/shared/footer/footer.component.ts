eb69f84d30ade4d5c9be0e5d604da77733d8d40d
Enrique Villasana <enrique.villasana26@gmail.com>
2020-07-29 21:43:43 -0400

    Changis in Navbar, Home and Footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styles: []
+  styles: ['footer { color: white; position: fixed; bottom: 0px; width: 100%; }']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  public year: number;
+
+  constructor() {
+    this.year = new Date().getFullYear();
+  }
 
   ngOnInit(): void {
   }
